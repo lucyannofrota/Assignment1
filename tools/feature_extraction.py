7eef53a656ef524f02aa8effbcdead263490653f
Lucyanno Frota <lucyannofrota@gmail.com>
2022-04-04 21:32:41 +0100

    Dataset Processing Tools

--- tools/feature_extraction.py
+++ tools/feature_extraction.py
@@ -5,14 +5,21 @@ import numpy as np
 def feature_SerializedImg(img):
     return img.flatten()
 
-# Color Histogram 256
-def feature_colorHistogram(img):
-    return cv2.calcHist([img], [0], None, [256], [0, 256])
-
 
 # Color Mean 1
 def feature_colorMean(img):
     return img.mean()
+
+
+# [Sum per row 28 | Sum per col 28 | Sum all 1] = 57
+def feature_SumImg(img):
+    return np.concatenate((img.sum(axis=1), img.sum(axis=0), [img.sum()]))
+
+
+# Color Histogram 256
+def feature_colorHistogram(img):
+    return cv2.calcHist([img], [0], None, [256], [0, 256])
+
 
 # [Sum noise per row 28 | Sum noise per col 28 | Sum noise all 1] = 57
 def feature_noise(img):
@@ -23,14 +30,14 @@ def feature_noise(img):
 
 # Serialized Bluried Img 28x28 = 784
 def feature_gBlur(img):
-    return feature_SerializedImg(cv2.GaussianBlur(img, (3, 3),0))
+    return cv2.GaussianBlur(img, (3, 3),0)
 
 
 # Serialized Median Img 28x28 = 784
 def feature_medianBlur(img):
-    return feature_SerializedImg(cv2.medianBlur(img, 3))
+    return cv2.medianBlur(img, 3)
 
 
 # Serialized Canny Img 28x28 = 784
 def feature_cannyEdge(img):
-    return feature_SerializedImg(cv2.cv2.Canny(img, 100, 200))
+    return cv2.Canny(img, 100, 200)
