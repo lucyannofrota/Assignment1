029d2db3c9caeb25b147f6112d238df2af69ab9b
Lucyanno Frota <lucyannofrota@gmail.com>
2022-04-05 12:52:15 +0100

    Processed Test Set added

--- tools/load_dataset.py
+++ tools/load_dataset.py
@@ -10,18 +10,25 @@ def load_imgs(path):
         imgs.append(img)
     return imgs
 
-def load_dataset(path):
-    fakePath = 'fake/'
-    realPath = 'real/'
 
-    fake_imgs = load_imgs(path+fakePath)
+def load_dataset(path, test=False):
+    if test:
+
+        imgs = np.array(load_imgs(path+"images/"))
+
+        labels = np.zeros((len(imgs), 1), dtype=bool)
+    else:
+        fakePath = 'fake/'
+        realPath = 'real/'
+
+        fake_imgs = load_imgs(path+fakePath)
 
 
-    fake_labels = np.zeros((len(fake_imgs), 1), dtype=bool)
-    real_imgs = load_imgs(path+realPath)
-    real_labels = np.ones((len(real_imgs), 1), dtype=bool)
-    # print(fake_labels)
+        fake_labels = np.zeros((len(fake_imgs), 1), dtype=bool)
+        real_imgs = load_imgs(path+realPath)
+        real_labels = np.ones((len(real_imgs), 1), dtype=bool)
+        # print(fake_labels)
 
-    imgs = np.concatenate((fake_imgs, real_imgs))
-    labels = np.concatenate((fake_labels, real_labels))
+        imgs = np.concatenate((fake_imgs, real_imgs))
+        labels = np.concatenate((fake_labels, real_labels))
     return imgs, labels
